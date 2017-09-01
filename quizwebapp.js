217154210eb6e49d8609ae0bffc05ebb17343606
lukeborders <31492762+lukeborders@users.noreply.github.com>
2017-09-01 10:04:33 -0700

    Update quizwebapp.js

--- quizwebapp.js
+++ quizwebapp.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-    
+$("input").click(function(){
+    $(".quiz-text").hide();
+    $("#button1").hide();
+    $("#button2").show();
+    $("#answer1").show();
+    $("#answer2").show();
+    $("#question1").show();
 });
-
-
+    $("#button2").click(function(){
+    $(".quiz-text").show();
+    $("#button1").show();
+    $("button2").hide();
+    });
+ /*$("#answer1","#answer2").click(function() {
+    if($("#answer1").click()){
+        $("question1").hide();
+        
+    }*/
+});
